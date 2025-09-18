9b3c6578bae548f88a0f952449b6f4fc6d754fdf
Dimas <dimas@kemaspkg.com>
2025-09-18 03:26:34 +0000

    pindahkan api firebase TS ke .env

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,13 +3,12 @@ import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
-  "projectId": "studio-3252810572-56c54",
-  "appId": "1:597994481713:web:0f4fb09e196be720ae0d10",
-  "storageBucket": "studio-3252810572-56c54.firebasestorage.app",
-  "apiKey": "AIzaSyCPUCPtcgntNFw2kHKrkGPJH4NYs9412CQ",
-  "authDomain": "studio-3252810572-56c54.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "597994481713"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
