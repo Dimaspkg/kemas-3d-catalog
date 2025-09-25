d25ba363923b0fcc22c797d14f6f5db39b44b488
Dimas <dimas@kemaspkg.com>
2025-09-25 09:00:35 +0000

    hapus (_for element div_)

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,7 +2,6 @@
 'use client';
 
 import Link from "next/link";
-import { Logo } from "@/components/icons/logo";
 import { Button } from "./ui/button";
 import { useEffect, useState } from "react";
 import { onAuthStateChanged, User } from "firebase/auth";
@@ -20,7 +19,7 @@ export default function Header() {
   }, []);
 
   return (
-    <header className="px-4 lg:px-8 py-8 border-b flex items-center justify-between bg-background z-20 sticky top-0">
+    <header className="px-4 lg:px-8 py-8 border-b flex items-center justify-end bg-background z-20 sticky top-0">
       <Link href="/" className="flex items-center gap-4">
         
       </Link>
@@ -36,31 +35,6 @@ export default function Header() {
                 <Link href="/canvas">Customize</Link>
             </Button>
         </nav>
-        <div className="flex items-center gap-4">
-            <Button variant="ghost" size="icon" className="text-muted-foreground hover:text-foreground">
-                <Search className="h-5 w-5" />
-                <span className="sr-only">Search</span>
-            </Button>
-            <Button variant="ghost" size="icon" className="text-muted-foreground hover:text-foreground">
-                <ShoppingBag className="h-5 w-5" />
-                <span className="sr-only">Cart</span>
-            </Button>
-            {user ? (
-              <Button variant="ghost" size="icon" asChild className="text-muted-foreground hover:text-foreground">
-                <Link href="/admin">
-                    <UserIcon className="h-5 w-5" />
-                    <span className="sr-only">Admin</span>
-                </Link>
-              </Button>
-            ) : (
-              <Button variant="ghost" size="icon" asChild className="text-muted-foreground hover:text-foreground">
-                <Link href="/login">
-                    <UserIcon className="h-5 w-5" />
-                    <span className="sr-only">Login</span>
-                </Link>
-              </Button>
-            )}
-        </div>
       </div>
     </header>
   );
