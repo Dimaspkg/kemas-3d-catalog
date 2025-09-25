73c95bf891e4aee8e3ec397a381b1c615d34cb3d
Dimas <dimas@kemaspkg.com>
2025-09-25 08:46:38 +0000

    perbesar ukuran header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -19,10 +19,10 @@ export default function Header() {
   }, []);
 
   return (
-    <header className="px-4 lg:px-8 py-3 border-b flex items-center justify-between bg-card shadow-sm z-20 relative">
+    <header className="px-4 lg:px-8 py-4 border-b flex items-center justify-between bg-card shadow-sm z-20 relative">
       <Link href="/" className="flex items-center gap-4">
-        <Logo className="h-8 w-8 text-primary-foreground" style={{ color: 'hsl(var(--primary))' }} />
-        <h1 className="text-2xl font-headline font-bold text-foreground">
+        <Logo className="h-10 w-10 text-primary-foreground" style={{ color: 'hsl(var(--primary))' }} />
+        <h1 className="text-3xl font-headline font-bold text-foreground">
           KEMAS Innovations
         </h1>
       </Link>
