0f7ea39512003ce228ee7f69a13f81b3c80b2e85
Dimas <dimas@kemaspkg.com>
2025-09-25 08:49:01 +0000

    buat header sticky menempel di atas layar

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -19,10 +19,10 @@ export default function Header() {
   }, []);
 
   return (
-    <header className="px-4 lg:px-8 py-4 border-b flex items-center justify-between bg-card shadow-sm z-20 relative">
+    <header className="px-4 lg:px-8 py-6 border-b flex items-center justify-between bg-card shadow-sm z-50 sticky top-0">
       <Link href="/" className="flex items-center gap-4">
-        <Logo className="h-10 w-10 text-primary-foreground" style={{ color: 'hsl(var(--primary))' }} />
-        <h1 className="text-3xl font-headline font-bold text-foreground">
+        <Logo className="h-12 w-12 text-primary-foreground" style={{ color: 'hsl(var(--primary))' }} />
+        <h1 className="text-4xl font-headline font-bold text-foreground">
           KEMAS Innovations
         </h1>
       </Link>
