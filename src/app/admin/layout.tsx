72ade1d720f1d98db3e488548f7ed44237ebf937
Dimas <dimas@kemaspkg.com>
2025-11-26 08:45:20 +0000

    [state=collapsed]:size-6

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -44,7 +44,7 @@ export default function AdminLayout({
          <SidebarProvider>
             <Sidebar variant="floating" collapsible="icon">
               <SidebarHeader className="items-start p-4">
-                  <HeaderLogo logoUrl={null} className="transition-all duration-200 group-data-[state=collapsed]:size-8" />
+                  <HeaderLogo logoUrl={null} className="transition-all duration-200 group-data-[state=collapsed]:size-6" />
               </SidebarHeader>
               <Separator />
               <SidebarContent className="p-2">
